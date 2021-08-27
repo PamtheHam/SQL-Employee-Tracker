745e008d1d3c64598406cd17a7951d9d663a9da4
Pamela Hammond <hammondpamelac@gmail.com>
2021-08-27 14:50:30 -0400

    installed console.table, adjusted schema tables, edited comments of readme

--- schema.sql
+++ schema.sql
@@ -3,21 +3,29 @@ CREATE DATABASE employeeTracker_db;
 
 USE employeeTracker_db;
 
+DROP TABLE IF EXISTS departments;
+CREATE TABLE departments (
+  id INT PRIMARY KEY,
+  name VARCHAR(30) NOT NULL,
+)
+
 DROP TABLE IF EXISTS roles;
 CREATE TABLE roles (
-  id INT NOT NULL,
-  title VARCHAR(100) NOT NULL,
-  salary INT NOT NULL,
+  id PRIMARY KEY,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL NOT NULL,
+  department_id INT NOT NULL,
   PRIMARY KEY,
 )
 
 DROP TABLE IF EXISTS employees;
 CREATE TABLE employees (
-  id INT NOT NULL,
+  id INT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
+  role_id INT NOT NULL,
+  manager_id INT NOT NULL,
   FOREIGN KEY (movie_id),
   REFERENCES movies(id),
   ON DELETE SET NULL
-
 )
